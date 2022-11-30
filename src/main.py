c0da9130741ad29306d5e247485649e5a36eab1d
Ben <24gluck_benjamin@dublinstudents.net>
2022-11-29 19:06:24 -0500

    adding dragging and moving pieces

--- src/main.py
+++ src/main.py
@@ -24,20 +24,30 @@ def main():
 
     grid = init_grid()
 
+    selected_tile = None
     while True:
+        hovered_tile = get_square_under_mouse(grid)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-
-
-        piece, x, y = get_square_under_mouse(grid)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if hovered_tile != None:
+                    selected_tile = hovered_tile
+            if event.type == pygame.MOUSEBUTTONUP:
+                drop_tile = get_square_under_mouse(grid)
+                if drop_tile:
+                    drop_tile.piece = selected_tile.piece
+                    selected_tile.piece = None
+                selected_piece = None
+                drop_pos = None
+                    
 
         draw_grid()
         render_pieces(grid)
 
-        if x != None:
-            rect = (BOARD_POS[0] + x * TILE_SIZE, BOARD_POS[1] + y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
+        if hovered_tile.x != None:
+            rect = (BOARD_POS[0] + hovered_tile.x * TILE_SIZE, BOARD_POS[1] + hovered_tile.y * TILE_SIZE, TILE_SIZE, TILE_SIZE)
             pygame.draw.rect(SCREEN, (255, 0, 0, 50), rect, 2)
 
         pygame.display.flip()
@@ -53,8 +63,8 @@ def init_grid():
     tiles = []
     grid = Grid(8, 8)
     for x in range(8):
-        tiles.append(Tile(x, 0, Piece(grid, "black", (x, 0))))
-        tiles.append(Tile(x, 7, Piece(grid, "white", (x, 7))))
+        tiles.append(Tile(0, x, Piece(grid, "black", (x, 0))))
+        tiles.append(Tile(7, x, Piece(grid, "white", (x, 7))))
     grid.add_pieces(tiles)
     return grid
 
@@ -73,9 +83,10 @@ def get_square_under_mouse(grid):
     mouse_pos = pygame.Vector2(pygame.mouse.get_pos()) - BOARD_POS
     x, y = [int(v // TILE_SIZE) for v in mouse_pos]
     try: 
-        if x >= 0 and y >= 0: return (grid.tiles[y][x], x, y)
+        if x >= 0 and y >= 0:
+            return Tile(x, y, grid.tiles[y][x].piece)
     except IndexError: pass
-    return None, None, None
+    return None
 
 if __name__ == '__main__':
     main()
